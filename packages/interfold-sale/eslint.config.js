d232e3dbac5c95a0d0e8c9a3d0bc603c2a75bb71
Hamza Khalid <hamzakhalid829@gmail.com>
2026-06-27 21:15:57 +0500

    fix: license

--- packages/interfold-sale/eslint.config.js
+++ packages/interfold-sale/eslint.config.js
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
