c25a3afb28abe69980f1fd61a8b1362a2e06ba91
0xjei <hello@0xjei.dev>
2026-09-22 18:16:41 +0200

    chore: pin support guest to compact l-BFV envelope

--- crates/support/contracts/ImageID.sol
+++ crates/support/contracts/ImageID.sol
@@ -19,5 +19,5 @@
 pragma solidity ^0.8.20;
 
 library ImageID {
-    bytes32 public constant PROGRAM_ID = bytes32(0x63a0ce97b23dd98a631dd47b80caf89286502db428a012bd206b4362abe20d80);
+    bytes32 public constant PROGRAM_ID = bytes32(0xe9f65c65fdf48ac315c36e7fc99514e2211704708238ae317c304f8f74ca65ba);
 }
