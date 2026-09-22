c81b8d43ba5c2f1a5c3e8e1a32c6d493b79db410
0xjei <hello@0xjei.dev>
2026-09-22 17:08:15 +0200

    chore: pin support guest to codec cutover

--- crates/support/contracts/ImageID.sol
+++ crates/support/contracts/ImageID.sol
@@ -19,5 +19,5 @@
 pragma solidity ^0.8.20;
 
 library ImageID {
-    bytes32 public constant PROGRAM_ID = bytes32(0x3699dbc34b70275b8b9a424e269aaf76fdab61c7a9cae6596f247fc7913d218e);
+    bytes32 public constant PROGRAM_ID = bytes32(0x63a0ce97b23dd98a631dd47b80caf89286502db428a012bd206b4362abe20d80);
 }
