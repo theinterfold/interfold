a9b9a086f4c3f3a5d40c205d4beb8b56b593d94d
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2026-09-24 22:57:27 +0100

    chore: coderabbit comments

--- crates/support/contracts/ImageID.sol
+++ crates/support/contracts/ImageID.sol
@@ -19,5 +19,5 @@
 pragma solidity ^0.8.20;
 
 library ImageID {
-  bytes32 public constant PROGRAM_ID = bytes32(0x9d3e21fd7cc08e629cb467e5ca6bfc8e1a645b483bfd91b6b1648ced259110fc);
+  bytes32 public constant PROGRAM_ID = bytes32(0xa38bb986e6c064ba05001b83fd40dccbaceec63aa397d4220ac25ad4d2c84338);
 }
